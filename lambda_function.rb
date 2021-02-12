9fb605e3638db7da0fac38373859a9453f012162
vlsk3104 <vlsk3104@gmail.com>
2021-02-12 16:47:32 +0900

    作成したGASを外部から呼ぶ

--- lambda_function.rb
+++ lambda_function.rb
@@ -41,4 +41,19 @@ def lambda_handler
   join_url = JSON.parse(res.body)['join_url']
 end
 
-pp lambda_handler
+def fetch_daily_person(uri)
+  uri = URI.parse(uri)
+  http = Net::HTTP.new(uri.hostname, uri.port)
+  req = Net::HTTP::Get.new(uri.request_uri)
+  http.use_ssl = true
+  res = http.request(req)
+
+  case res
+  when Net::HTTPOK
+    res.body.force_encoding("UTF-8")
+  when Net::HTTPFound
+    fetch_daily_person(res["location"])
+  end
+end
+
+pp fetch_daily_person(ENV['SPREAD_SHEET_URL'])
