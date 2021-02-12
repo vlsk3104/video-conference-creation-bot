96c9f3b2050049c4e61b57e851482fb80fc708c8
vlsk3104 <vlsk3104@gmail.com>
2021-02-12 13:27:50 +0900

    会議室を予約

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,8 +1,11 @@
+require 'dotenv'
 require 'net/http'
 require "json/add/core"
 
+Dotenv.load
+
 def lambda_handler
-  path = "https://api.zoom.us/v2/users/vlsk3104@gmail.com/meetings"
+  path = "https://api.zoom.us/v2/users/#{ENV['USER_ID']}/meetings"
   uri = URI.parse(path)
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -16,9 +19,18 @@ def lambda_handler
     agenda: "進捗報告"
   }.to_json
 
+  headers = {
+    "Content-Type" => "application/json",
+    "Authorization" => "Bearer #{ENV['JWT']}"
+  }
+
   req = Net::HTTP::Post.new(uri.path)
   req.body = payload
+
+  req.initialize_http_header(headers)
 
   res =http.request(req)
   join_url = JSON.parse(res.body)['join_url']
 end
+
+pp lambda_handler
